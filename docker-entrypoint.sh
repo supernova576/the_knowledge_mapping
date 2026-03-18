56d556eef20837b54b2a28934447a04367a49b00
YNSJK <yaniskla3a@gmail.com>
2026-03-18 19:16:33 +0100

    fixes docker-entrypoint.sh script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ cp /tmp/github_key /run/secrets/github_key
 chmod 400 /run/secrets/github_key
 
 # rm old app-logs
-rm /app/logs/app.log
+rm /app/logs/app.log | echo -e "Could not remove logs... Continue"
+
+# rm exports
+rm -rf /app/output/exports/* | echo -e "Could not delete exports... Continue"
 
 exec "$@"
